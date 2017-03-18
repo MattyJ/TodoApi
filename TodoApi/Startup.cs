460a43b3dd4e5d8acf4674b69f6545ffee1ce072
Matthew Jordan <matthew.jordan@uk.fujitsu.com>
2017-03-18 14:41:17 +0000

    Entity Framework additions
    Microsoft.EntityFrameworkCore.Design and Core Tools added to allow me to
    add migrations.

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -28,6 +29,11 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add framework services.
+            services.AddDbContext<TodoContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+
             // Add framework services.
             services.AddMvc();
 
